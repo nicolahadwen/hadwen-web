a4855c4a5a8772108102f4d3dacea6a7e2d7dd28
nicolahadwen <nicola@ritual.co>
2019-12-30 13:52:34 -0500

    add tagging

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn package && docker build --build-arg JAR_FILE=target/HadwenWeb*.jar -t co/hadwen/web .
+mvn package && docker build --build-arg JAR_FILE=target/HadwenWeb*.jar -t co/hadwen/web:hadwen-web-v1 .
